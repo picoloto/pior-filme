632f84551212ed703deb9388092ad5c31200e245
Leandro Picoloto <picolotoleandro@gmail.com>
2023-12-03 15:39:36 -0300

    Added structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './module/movie-list/view/movie-list.component';
+import { DashboardComponent } from './module/dashboard/view/dashboard.component';
 
 export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'movie-list', component: MovieListComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: DashboardComponent }, // Normalmente utilizaod para pagina não encontrada, mas vou implementar para voltar ao inicio.
 ];
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  @Input() isMobile = false;
+  @Output() changeMenuStatus = new EventEmitter<void>();
+
+  menuClick(): void {
+    this.changeMenuStatus.emit();
+  }
+}
